2f002f8575d199c5171f4e778b15bbb76c98dcb0
Den <clixqqk@gmail.com>
2024-10-22 15:34:36 +0300

    fdfd

--- src/createServer.js
+++ src/createServer.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
-/* eslint-disable padding-line-between-statements */
 const http = require('http');
 const { convertToCase } = require('./convertToCase');
+
 const createServer = () => {
   return http.createServer((req, res) => {
     const url = new URL(`http://${process.env.HOST ?? 'localhost'}${req.url}`);
@@ -9,47 +9,32 @@ const createServer = () => {
     const originalText = url.pathname.slice(1);
     const targetCase = url.searchParams.get('toCase');
 
+    const errors = [];
+
     if (!originalText) {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
-      res.end(
-        JSON.stringify({
-          errors: [
-            {
-              message:
-                'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>"',
-            },
-          ],
-        }),
-      );
-      return;
+      errors.push({
+        message:
+          'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
     }
 
     if (!targetCase) {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
-      res.end(
-        JSON.stringify({
-          errors: [
-            {
-              message:
-                '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>"',
-            },
-          ],
-        }),
-      );
-      return;
+      errors.push({
+        message:
+          '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    } else if (
+      !['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'].includes(targetCase)
+    ) {
+      errors.push({
+        message: `This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.`,
+      });
     }
 
-    if (!['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'].includes(targetCase)) {
+    if (errors.length > 0) {
       res.writeHead(400, { 'Content-Type': 'application/json' });
-      res.end(
-        JSON.stringify({
-          errors: [
-            {
-              message: `This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.`,
-            },
-          ],
-        }),
-      );
+      res.end(JSON.stringify({ errors }));
+
       return;
     }
 
